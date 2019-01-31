f7150c837c994fcfc2c6f2c5c340b39e66578dbd
larynab <fsovreign@gmail.com>
2019-01-31 14:00:32 -0800

    main-methods

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -13,15 +13,13 @@ class TabLink {
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
     this.tabItem = new TabItem(this.itemElement);
-    
     // Add a click event listener on this instance, calling the select method on click
-
+    element.addEventListener('click', () =>this.select());
   };
-
-  select() {
+    select() {
     // Get all of the elements with the tabs-link class
     // const links;
-
+    const links = getElementsByClassName('tabs-link');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
 
@@ -60,4 +58,5 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+let links = document.querySelectorAll('.tabs-link');
+links.forEach( element => new TabLink(element));
